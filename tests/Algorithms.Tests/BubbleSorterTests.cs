3088083dece16ad53e4c4a4a6495886233201eb9
Andrey Dorokhov <aidmsu@gmail.com>
2018-03-21 11:21:23 +0300

    Fix bubble sorter test

--- tests/Algorithms.Tests/BubbleSorterTests.cs
+++ tests/Algorithms.Tests/BubbleSorterTests.cs
@@ -16,7 +16,7 @@ namespace Algorithms.Benchmarks
         [Fact]
         public void BubbleSorter_Sort2()
         {
-            var result = new [] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}.BubbleSort();
+            var result = new [] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}.BubbleSort();
             Assert.Equal(new [] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, result);
         }
     }
